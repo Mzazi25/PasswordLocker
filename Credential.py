751f0c5dfb38453acf37fa6a8036979f91d58912
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-02-18 23:30:07 +0300

    Updated Credential Class and defined credential names and passwords

---
+++ Credential.py
@@ -0,0 +1,10 @@
+class Credential:
+    """
+    Added Crediatial class
+    """
+    def __init__(self,credential_name, credential_password):
+        """
+        Def_Init to define credential_names and credential_passwords
+        """
+        self.credential_name= credential_name
+        self.credential_password= credential_password
