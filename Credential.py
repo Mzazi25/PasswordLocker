3bf5e559e1853d13bb4787447f8ad8e6738c5f12
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-02-18 23:40:27 +0300

    Added credential exists method

--- Credential.py
+++ Credential.py
@@ -1,3 +1,6 @@
+from operator import truediv
+
+
 class Credential:
     """
     Added Crediatial class
@@ -10,8 +13,36 @@ class Credential:
         """
         self.credential_name= credential_name
         self.credential_password= credential_password
+
     def save_credential(self):
         """
         save credential details to the credential_list
         """
         Credential.credential_list.append(self)
+    
+    def delete_credential(self):
+        """
+        save credential details to the credential_list
+        """
+        Credential.credential_list.remove(self)
+
+    @classmethod
+    def find_by_credential(cls,string):
+        """
+        Method that takes in a string and returns the credential details that matches the account
+        """
+        for credential in cls.credential_list:
+            if credential.credential_name == string:
+                return credential
+
+    @classmethod
+    def credential_exists(cls,string):
+        """
+        Method that takes in a string and returns a boolian if the credential exists
+        """
+        for credential in cls.credential_list:
+            if credential.credential_name == string:
+                return True
+        return False
+
+    
