9679a4545b6b9a46ba6d0db44142b4ce87d298f3
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-02-18 23:34:18 +0300

    added save_credential method

--- Credential.py
+++ Credential.py
@@ -2,9 +2,16 @@ class Credential:
     """
     Added Crediatial class
     """
+    credential_list=[] #Empty Array to append creditials
+
     def __init__(self,credential_name, credential_password):
         """
-        Def_Init to define credential_names and credential_passwords
+        def_init to define credential_names and credential_passwords
         """
         self.credential_name= credential_name
         self.credential_password= credential_password
+    def save_credential(self):
+        """
+        save credential details to the credential_list
+        """
+        Credential.credential_list.append(self)
