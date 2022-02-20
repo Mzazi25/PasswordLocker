68318a47c41b4daf1f2df3687bc9c3f942029512
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-02-20 16:29:53 +0300

    Updated test_init

--- test.py
+++ test.py
@@ -16,3 +16,16 @@ class TestCredentialandUser(unittest.TestCase):
         """
         self.new_user= User("Caleb","Langat90")
         self.new_credential= Credential("Twitter","Mzazi","Langat25")
+    
+    def test_init(self):
+
+        """
+        test_init method to test if the object is initialized well
+        """
+        self.assertEqual(self.new_user.user_name,"Caleb")
+        self.assertEqual(self.new_user.user_password,"Langat90")
+        self.assertEqual(self.new_credential.app_name,"Twitter")
+        self.assertEqual(self.new_credential.credential_user_name,"Mzazi")
+        self.assertEqual(self.new_credential.credential_password,"Langat25")
+
+
